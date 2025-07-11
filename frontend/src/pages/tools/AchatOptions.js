4e337da40f56c47085ea562bea23e823a7385dad
Saif <daghsnisaif@gmail.com>
2025-07-11 15:29:59 +0100

    transaction 70%  11/07/2025

--- frontend/src/pages/tools/AchatOptions.js
+++ frontend/src/pages/tools/AchatOptions.js
@@ -1,82 +1,109 @@
-import React, { useState } from 'react';
-import './AchatOptions.css';
+import React, { useState } from "react";
+import "./AchatOptions.css";
 // eslint-disable-next-line no-unused-vars
-import { FiCheck } from 'react-icons/fi';
-import Consulter from './Consulter';
+import { FiCheck } from "react-icons/fi";
+import Consulter from "./Consulter";
+import PropTypes from 'prop-types';
+import Vente from "../tools/Vente"
+
 
 const AchatOptions = (props) => {
   const [details, setdetails] = useState(false);
   const [consulter, setConsulter] = useState(false);
   const [confirme, setConfirme] = useState(true);
+  const [modifier, setmodifier] = useState(false);
 
   return (
-    <div className='option'>
-      <div className='row'>
-        <div className='option-photo'>
-          <img src='transactions/profile.png' alt='profile' />
+    <div className="option">
+      <div className="row">
+        <div className="option-photo">
+          <img src="transactions/profile.png" alt="profile" />
         </div>
 
-        <div className='option-details'>
-          <label className='option-details-title'>Demande de Tony Reichert</label>
+        <div className="option-details">
+          <label className="option-details-title">
+            Demande de Tony Reichert
+          </label>
 
-          <div className='option-details-row'>
-            <label>Type :</label>
+          <div className="option-details-row">
+            <label>Type    :</label>
             <a>Matiere premiers</a>
           </div>
 
-          <div className='option-details-row'>
+          <div className="option-details-row">
             <label>Gamme :</label>
             <a>Moyenne gamme</a>
           </div>
 
-          <div className='option-details-row'>
-            <label>Quantité :</label>
+          <div className="option-details-row">
+            <label>Quantité:</label>
             <a>40</a>
-          {/* <p className='option-details-nego'> Négociable</p> */}
           </div>
 
-          <div className='option-details-row'>
-            <label>Prix :</label>
+          <div className="option-details-row">
+            <label>Prix        :</label>
             <a>200.000 dt</a>
-          {/* <p className='option-details-nego'> Négociable</p> */}
           </div>
-
         </div>
       </div>
+            {/* <p className='option-details-nego'> Négociable</p> */}
 
+            {/* <p className='option-details-nego'> Négociable</p> */}
       {consulter && (
         <Consulter setConfirme={setConfirme} setConsulter={setConsulter} />
       )}
 
       {confirme ? (
-        <div className='option-details-row-buttons'>
+        <div className="option-details-row-buttons">
           <button
-            className='option-details-Consulter-Consulter'
-            onClick={() => setConsulter(true)}
+            className="option-details-Consulter-Consulter"
+            onClick={() => {
+              if (props.button === "Consulter") {
+                setConsulter(true);
+              } else if (props.button === "Modifier") {
+                setmodifier(!modifier);
+              }
+            }}
           >
             {props.button}
           </button>
         </div>
       ) : (
-        <div className='Consulter'>
-          <div className='Consulter-row'>
-            <FiCheck className='Consulter-icon' />
+        <div className="Consulter">
+          <div className="Consulter-row">
+            <FiCheck className="Consulter-icon" />
             <p>
-              La demande de vente a été confirmé par vous, confirmant ainsi sa participation à la commande.
+              La demande de vente a été confirmé par vous, confirmant ainsi sa
+              participation à la commande.
             </p>
           </div>
         </div>
       )}
 
-      {details && <div className='overlay'></div>}
+      {details  && <div className="overlay"></div>}
 
       {details && (
-        <div className='option-details-details'>
+        <div className="option-details-details">
           <button onClick={() => setdetails(false)}>go back</button>
         </div>
       )}
+
+      {modifier &&(
+        
+                          <>
+                            <Vente
+                            title={"Modifier"}
+                            confirme={"Mettre à jour "}
+                            setmodifier={setmodifier}
+                            />
+                          </>
+      )}
     </div>
   );
+};
+AchatOptions.propTypes = {
+  button: PropTypes.string,
+  setmodifier: PropTypes.func,
 };
 
 export default AchatOptions;
--- frontend/src/pages/tools/Vente.jsx
+++ frontend/src/pages/tools/Vente.jsx
@@ -1,9 +1,289 @@
-import React from 'react'
+import React, { useState } from 'react';
+//import { FiPlus, FiMinus } from 'react-icons/fi';
+import Select from 'react-select';
+import PropTypes from 'prop-types';
+/*
+function Input_vente(props) {
+  const [value, setValue] = useState('');
 
-const Vente = () => {
+  const handleChange = (e) => {
+    const val = e.target.value;
+    if (val === '' || /^[0-9]+$/.test(val)) {
+      setValue(val);
+    }
+  };
+
+  const increment = () => {
+    setValue((prev) => {
+      const num = parseInt(prev) || 0;
+      return num + 1;
+    });
+  };
+
+  const decrement = () => {
+    setValue((prev) => {
+      const num = parseInt(prev) || 0;
+      return num > 0 ? num - 1 : 0;
+    });
+  };
+
   return (
-    <div>Vente</div>
+    <div className='new-div-input'>
+      <input
+        className='new-vente-input'
+        // eslint-disable-next-line react/prop-types
+        placeholder={props.name}
+        type='text'
+        value={value}
+        onChange={handleChange}
+      />
+      <div className='Pricerow'>
+        <button className='the-button' type='button' onClick={decrement}>
+          <FiMinus size={18} />
+        </button>
+        <button className='the-button' type='button' onClick={increment}>
+          <FiPlus size={18} />
+        </button>
+      </div>
+    </div>
+  );
+}*/
+
+const customStyles = {
+  control: (provided) => ({
+    ...provided,
+    height: '43px',
+    marginTop: '5px',
+    outline: 'none',
+    paddingLeft: '8px',
+    paddingRight: '8px',
+    borderRadius: '8px',
+    border: '2px solid #E4E4E7',
+    fontWeight: 500,
+    fontSize: '16px',
+    display: 'flex',
+    alignItems: 'center',
+    boxShadow: 'none',
+    '&:hover': {
+      borderColor: 'none',
+    },
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected
+      ? '#CDE3F2'
+      : state.isFocused
+      ? '#CDE3F2'
+      : '#fff',
+    color: '#000',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    margin: 0,
+    padding: 0,
+    lineHeight: '36px',
+    color: '#E4E4E7 !important',
+  }),
+  input: (provided) => ({
+    ...provided,
+    margin: 0,
+    padding: 0,
+    lineHeight: '36px',
+  }),
+};
+
+const gammeOptions = [
+  { value: 'Entrée de gamme', label: 'Entrée de gamme' },
+  { value: 'Moyenne gamme', label: 'Moyenne gamme' },
+  { value: 'Haut gamme', label: 'Haut gamme' },
+];
+
+const typeoptions = [
+  { value: 'Matières premières', label: 'Matières premières' },
+  { value: 'Emballage', label: 'Emballage' },
+  { value: 'Produits fini', label: 'Produits fini' },
+  { value: 'Machines', label: 'Machines' },
+];
+
+const Vente = (props) => {
+    
+  const [prixNego, setPrixNego] = useState(false);
+  const [quantite, setQuantite] = useState('');
+  const [prix, setPrix] = useState('');
+  const [quantiteNego, setQuantiteNego] = useState(false);
+  const [type, setType] = useState('');
+  const [gamme, setGamme] = useState('');
+  const [publier, setPublier] = useState(false);
+
+
+  const handleChangeType = (option) => {
+    setType(option);
+  };
+
+  const handleChangeGamme = (option) => {
+    setGamme(option);
+  };
+
+  return (
+    <div className='transaction-Bottom-vente'>
+      <label className='vente-title'>{props.title}</label>
+
+      <label className='vente-label'>Type</label>
+      <Select
+        options={typeoptions}
+        value={type}
+        onChange={handleChangeType}
+        name='type'
+        styles={customStyles}
+        placeholder='Select a type'
+      />
+
+      <label className='vente-label'>Gamme</label>
+      <Select
+        options={gammeOptions}
+        value={gamme}
+        onChange={handleChangeGamme}
+        name='gamme'
+        styles={customStyles}
+        placeholder='Select a gamme'
+      />
+
+      {/* debut Quantité & Prix */}
+      <div className='quantite-prix-block'>
+        <label className='vente-label'>Quantité</label>
+        <div className='Pricerow'>
+          <input
+            className='vente-input-prix'
+            type='number'
+            placeholder='Quantité'
+            onChange={(e) => setQuantite(e.target.value)}
+          />
+          <div className='prixdiv'>
+            <label className='custom-checkbox'>
+              <input type='checkbox' className='prixcheckbox' name='agree' />
+              <span
+                className='checkmark'
+                onClick={() => setQuantiteNego(!quantiteNego)}
+              ></span>
+              <span className='Prixlabel'>Négociable</span>
+            </label>
+          </div>
+        </div>
+
+        <label className='vente-label'>Prix</label>
+        <div className='Pricerow'>
+          <input
+            className='vente-input-prix'
+            type='number'
+            placeholder='Prix'
+            onChange={(e) => setPrix(e.target.value)}
+          />
+          <div className='prixdiv'>
+            <label className='custom-checkbox'>
+              <input type='checkbox' className='prixcheckbox' name='agree' />
+              <span
+                className='checkmark'
+                onClick={() => setPrixNego(!prixNego)}
+              ></span>
+              <span className='Prixlabel'>Négociable</span>
+            </label>
+          </div>
+        </div>
+      </div>
+      {/* fin Quantité & Prix */}
+
+      <div className='transaction-Bottom-vente-buttons'>
+        <button
+          className='transaction-Bottom-vente-buttons1'
+          onClick={() => {
+              if (props.title === "Vente" || props.title === "Achat") {
+                props.setvente(false);
+              } else if (props.title === "Modifier") {
+                props.setmodifier(false);
+              }
+            }}
+        >
+          Annuler
+        </button>
+        <button
+          className='transaction-Bottom-vente-buttons2'
+          onClick={() => setPublier(!publier)}
+        >
+          Publier
+        </button>
+      </div>
+
+      {/* debut confirmation */}
+
+      {publier && <div className='overlay'></div>}
+
+      {publier && (
+        <div className='confirmation'>
+          <div className='confirmation-container'>
+            <div className='Pricerow'>
+              <label className='confirmation-label'>Type :</label>
+              <a>{type ? type.label : ''}</a>
+            </div>
+
+            <div className='Pricerow'>
+              <label className='confirmation-label'>Gamme :</label>
+              <a>{gamme ? gamme.label : ''}</a>
+            </div>
+
+            <div className='Pricerow1'>
+              <div className='Pricerow'>
+                <label className='confirmation-label'>
+                  Quantité :
+                </label>
+                <a>{quantite}</a>
+              </div>
+              {quantiteNego ? (
+                <a className='nego'>Négociable</a>
+              ) : (
+                <a className='Pasnego'>Pas négociable</a>
+              )}
+            </div>
+
+            <div className='Pricerow1'>
+              <div className='Pricerow'>
+                <label className='confirmation-label'>Prix :</label>
+                <a>{prix} dt</a>
+              </div>
+              {prixNego ? (
+                <a className='nego'>Négociable</a>
+              ) : (
+                <a className='Pasnego'>Pas négociable</a>
+              )}
+            </div>
+
+            <div className='Pricerow'>
+              <button
+                className='transaction-Bottom-vente-buttons1'
+                onClick={() => setPublier(!publier)}
+              >
+                cancel
+              </button>
+              <button className='transaction-Bottom-vente-buttons2'>
+                confirme
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Fin confirmation */}
+    </div>
   )
 }
 
-export default Vente
+Vente.propTypes = {
+  setvente: PropTypes.func.isRequired,
+  setmodifier: PropTypes.func, // Add this line for setmodifier prop validation
+  modifier: PropTypes.any, // Add this line for modifier prop validation
+  title: PropTypes.string.isRequired,
+};
+
+export default Vente;
+
