b4026fd6cc763a24c237fade86bf7aeb899d6036
Saif <daghsnisaif@gmail.com>
2025-07-25 08:58:51 +0100

    research user

--- frontend/src/pages/tools/Users/User.js
+++ frontend/src/pages/tools/Users/User.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./User.css";
 import { FaSearch } from "react-icons/fa";
 import UserProfile from "./UserProfile";
+import { handleError } from "../../../utils";
 
 const User = (props) => {
   const [selected, setselected] = useState(false);
   const [users, setUsers] = useState([]);
+  const [researh, setresearh] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/getAllUsers")
@@ -21,27 +24,52 @@ const User = (props) => {
       .catch((err) => console.error("❌ Fetch Users error:", err));
   }, []);
 
+  useEffect(() => {
+    const result = users.filter((user) =>
+      user.name.toLowerCase().includes(researh.toLowerCase())
+    && user.name !== props.user.name
+    );
+    if (result === "") {
+      return handleError("Utilisateur non trouvé");
+    }
+    setFilteredUsers(result);
+  }, [researh, users]);
   return (
     <div className="users-container">
       <div className="input-wrapper">
         <FaSearch className="search-icon" />
-        <input type="text" className="user-input" placeholder="Recherche" />
+        <input
+          type="text"
+          className="user-input"
+          placeholder="Recherche"
+          onChange={(e) => setresearh(e.target.value)}
+        />
       </div>
       <div className="users-buttom">
-        {users.map((user) => {
-          if (props.user._id === user._id) {
-            return null;
-          }
-          return (
-            <UserProfile
-              key={user._id}
-              id={user._id}
-              selected={selected === user._id}
-              onClick={() => setselected(user._id)}
-              name={user.name}
-            />
-          );
-        })}
+        {researh
+          ? filteredUsers.map((user) => (
+              <UserProfile
+                key={user._id}
+                id={user._id}
+                selected={selected === user._id}
+                onClick={() => setselected(user._id)}
+                name={user.name}
+              />
+            ))
+          : users.map((user) => {
+              if (props.user._id === user._id) {
+                return null;
+              }
+              return (
+                <UserProfile
+                  key={user._id}
+                  id={user._id}
+                  selected={selected === user._id}
+                  onClick={() => setselected(user._id)}
+                  name={user.name}
+                />
+              );
+            })}
       </div>
     </div>
   );
